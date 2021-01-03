49798028aa7248d67a36e9b6887284e27b5e5721
gyanendra basnet <bgyanendra93@gmail.com>
2021-01-03 10:33:18 -0600

    initial commit

---
+++ ConsoleAppCsharpPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleAppCsharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine(Math.Round(3.5));
+            Console.WriteLine(Math.Round(11.5));
+            Console.ReadLine();*/
+
+            int[] i = new int[0];
+            Console.WriteLine(i[0]);
+            Console.ReadLine();
+        }
+    }
+}
